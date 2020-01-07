e66cb60f309ecbd608b1d826dbad39f90111ae29
hansaemgo <rhgkstoa7@gmail.com>
2020-01-07 18:28:22 +0900

    03. 연산자
    산술연산자와 대입연산자

---
+++ src/연산자.js
@@ -0,0 +1,29 @@
+let value = 1; // 변수 선언
+value = 2; // 대입 연산자
+console.log(value);
+
+let a = 1 + 2 + 3 + 4;
+console.log(a);
+
+let b = 1 + 2 - (3 * 4) / 4;
+console.log(b);
+
+let c = 1;
+console.log(c++); //1
+console.log(c); //2
+console.log(++c); //3
+
+let d = 1;
+console.log(d--);
+console.log(d);
+console.log(--d);
+
+let e = 1;
+e += 3;
+console.log(e);
+e -= 3;
+console.log(e);
+e *= 9;
+console.log(e);
+e /= 3;
+console.log(e);
