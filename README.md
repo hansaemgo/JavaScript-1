f8e673071ec979d8bd97e2dad901c6fa35527054
hansaemgo <rhgkstoa7@gmail.com>
2020-01-07 17:56:50 +0900

    02. 변수와 상수
    변수와 상수 그리고 데이터 타입

--- README.md
+++ README.md
@@ -4,5 +4,15 @@
 
 1. 개발자도구 -> Console탭 사용
 2. console.log는 특정내용을 출력하는 방법
+    - 문자열, 숫자, 연산가능 ex) console.log(1+2+3+4);
 
-- 문자열, 숫자, 연산가능 ex) console.log(1+2+3+4);
+### 02. 변수(Variable) & 상수(Constant)
+
+1. 변수선언(바뀔수 있는 값) : let value = 1;
+2. 상수선언(값이 고정적) : const a = 1;
+3. var 권장하지 않는다(같은 이름으로 여러번 선언 가능)
+4. 데이터타입
+    - 숫자(Number)
+    - 문자열(String)
+    - 참/거짓(Boolean) -> ture/false
+    - null(진짜없다) undefined(아직정해지지않았다)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import "./styles.css";
+console.log('hello');
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let value = 1;
+console.log(value);
+value = 2;
+console.log(value);
+// let value = 2;
+
+const a = 1;
+console.log(a);
+// const a = 2;
+// 같은 이름으로 변수와 상수둘다  설정 안됨
+
+let number = 1;
+console.log(number);
+
+let text = '문자열';
+console.log(text);
+
+// let good = true;
+// let loading = false;
+
+const friend = null;
+console.log(friend);
+
+let criminal;
+console.log(criminal);
