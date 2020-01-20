0033e2dc25a5c1c23c5fda4158ce6b1057e91708
hansaemgo <rhgkstoa7@gmail.com>
2020-01-20 13:05:05 +0900

    setTimeout 에서 callback함수 사용하기
    setTimeout 에서 callback함수 사용하기

--- src/asynchronous.js
+++ src/asynchronous.js
@@ -15,17 +15,36 @@
 // 비동기적 처리 (첫번째 파라미터 : 함수, 두번째 파라미터 : 시간)
 // setTimeout : 시간 0 -> 브라우저 기본값이 4
 
-function work() {
+// function work() {
+//     setTimeout(() => {
+//         const start = Date.now();
+//         for (let i = 0; i < 1000000000; i++) {}
+//         const end = Date.now();
+//         console.log(end - start + 'ms');
+//     }, 0);
+// }
+
+// console.log('작업시작');
+// work();
+// console.log('다음작업');
+
+// 진행순서 작업시작출력 -> work 시작 -> 다음작업출력 -> work끝나서 값나옴
+
+// 3. work()함수가 끝나고 어떤 작업을 실행시키고 싶다면 callback함수를 파라미터로 넘겨준다
+
+function work(callback) {
     setTimeout(() => {
         const start = Date.now();
         for (let i = 0; i < 1000000000; i++) {}
         const end = Date.now();
         console.log(end - start + 'ms');
+        callback(end - start);
     }, 0);
 }
 
-console.log('작업시작');
-work();
+console.log('작업시작!');
+work(ms => {
+    console.log('작업이 끝났어요!');
+    console.log(ms + 'ms 걸렸다고 해요');
+});
 console.log('다음작업');
-
-// 진행순서 작업시작출력 -> work 시작 -> 다음작업출력 -> work끝나서 값나옴
